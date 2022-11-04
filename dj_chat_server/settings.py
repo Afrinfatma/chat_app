127abbb579f01d5a03c8ec4ff3ca01eb99559df0
Afrinfatma <afrinfatma4@gmail.com>
2022-11-04 14:54:17 +0530

    Added: chat layers for two way chatting

--- dj_chat_server/settings.py
+++ dj_chat_server/settings.py
@@ -70,6 +70,14 @@ TEMPLATES = [
 ]
 
 ASGI_APPLICATION = "dj_chat_server.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
